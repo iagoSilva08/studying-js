5ffce60dc7ca0bf213554d13746529b0da24a5db
iagoSilva08 <101933249+iagoSilva08@users.noreply.github.com>
2022-08-16 15:42:01 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,5 @@
-function calculadora(numberA, numberB) {
-  if('**'){
+function calculadora(numberA, numberB, operation){
+  if(operation ==='**'){
     return  numberA ** numberB
   }
   else if(operation === '-'){
