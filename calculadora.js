b10925d576440929d60f149a2c27a8126ced29ab
iagoSilva08 <101933249+iagoSilva08@users.noreply.github.com>
2022-08-16 15:20:58 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+function calculadora(valueA, valueB, operation) {
+  if(opereration === '**'){
+    return  valueA ** valueB
+  }
+  else if(operation === '-'){
+    return valueA - valueB
+  }
+  if (operation === '*'){
+    return  valueA * valueB
+  }
+  if (operation === '+'){
+    return valueA + valueB
+  }
+  if (operation === '/'){
+    return / valueA // valueB
+  }
+  const result = calculadora(2+2)
+  console.log(result)
