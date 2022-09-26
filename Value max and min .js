72792a156673eb783c6fc00d0120e19c78c36393
iagoSilva08 <101933249+iagoSilva08@users.noreply.github.com>
2022-09-26 13:29:42 -0300

    Update Value max and min .js

--- Value max and min .js
+++ Value max and min .js
@@ -1,12 +1,12 @@
 function maxandmin (numberA, numberB){
   if(numberA > numberB) {
-    console.log(`value max = ${numberA} ,value min = ${numberB}`)
+   console.log(`value max = ${numberA} ,value min = ${numberB}`)
   }
   else if(numberA < numberB) {
-     console.log(`value max = ${numberB} ,value min = ${numberA}`)
+   console.log(`value max = ${numberB} ,value min = ${numberA}`)
   }
   if(numberA === numberB){
-  console.log('values are the same')
+   console.log('values are the same')
   }
 };
 maxandmin(3,30)
